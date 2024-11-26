9da87638f0743caaf5df30b54b37afaed4284668
Manuel Chalá <manuel@theyoungastronauts.com>
2024-11-26 10:06:36 -0500

    Title change

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: 'Dashhost Example',
       routerConfig: _router,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
